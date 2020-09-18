bc4f477b2a84a157eb97cb98ee44e19fcff9f4bc
J K <kj688895@gmail.com>
2020-09-18 19:37:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -62,6 +62,13 @@ sum_array([11,4,7,8,9,100,134])
 
 
 def add_s(array)
-
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
 
 add_s(["hand", "feet", "knee", "table"])
