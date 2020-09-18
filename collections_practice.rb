e735898336397dc31e3bd1d4dac857ddce3cbac0
J K <kj688895@gmail.com>
2020-09-18 18:56:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,5 +25,7 @@ sort_array_char_count = ["dogs", "cat", "Horses"]
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
+  array
+end 
 
   swap_elements(["blake", "ashley", "scott"])
